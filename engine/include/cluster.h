2389d03cad47ece5dd0c7f8105bcb3f5066680c5
ZSShen <andy780328@hotmail.com>
2014-11-28 09:40:19 -0800

    Impl-Cluster: Code the definition of clustering configuration.

---
+++ engine/include/cluster.h
@@ -0,0 +1,23 @@
+#ifndef _CLUSTER_H_
+#define _CLUSTER_H_
+
+
+/* This ds records the paramters to control the clustering process. */
+typedef struct _CONFIG_T {
+    uint8_t ucCntThrd;      /* The number of threads used for parallel processing. */
+    uint8_t ucScoreSim;     /* The threshold to group similar binary slices. */
+    uint8_t ucCntBlk;       /* The number of hex byte blocks in pattern. */
+    uint8_t ucSizeBlk;      /* The size of the hex byte block. */    
+    uint8_t ucSizeSlc;      /* The size of the binary slice extracted from file
+                               which is the basic unit of clustering process. */
+    char *szPathRootIn;     /* The root pathname of the input sample set. */
+    char *szPathRootOut;    /* The root pathname of the output patterns. */
+    char *szPluginSlc;      /* The name of the plugin which is to be used for 
+                               file slicing. */
+    char *szPluginSim;      /* The name of the plugin which is to be used for 
+                               similarity comparison.
+    similarity */
+} CONFIG;
+
+
+#endif
