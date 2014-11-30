ab99b363e65c76e8211c454532e89d2fbcd0ad50
ZSShen <andy780328@hotmail.com>
2014-11-30 00:26:46 -0800

    Impl-Sim: Code the SSDeep similarity plugin.

--- plugin/similarity/include/similarity.h
+++ plugin/similarity/include/similarity.h
@@ -5,6 +5,18 @@
 #include <stdint.h>
 
 
+#define FAIL_MEM_ALLOC_HASH_STRING      "Fail to allocate buffer for hash string"
+#define FAIL_EXTERNAL_LIBRARY_CALL      "Fail to call the library for similarity computation"
+
+
+enum {
+    RC_SUCCESS = 0,
+    RC_FAIL_FILE_IO = -1,
+    RC_FAIL_MEM_ALLOC = -2,
+    RC_FAIL_LIBRARY_CALL = -3
+};
+
+
 /**
  * This function initializes the similarity comparison plugin.
  *
@@ -36,7 +48,7 @@ SimDeinit();
  * @return status code
  */
 int8_t
-SimGetHash(char *szBin, uint32_t uiLenBuf, char *szHash, uint32_t *p_uiLenHash);
+SimGetHash(char *szBin, uint32_t uiLenBuf, char **p_szHash, uint32_t *p_uiLenHash);
 
 
 /**
@@ -46,12 +58,14 @@ SimGetHash(char *szBin, uint32_t uiLenBuf, char *szHash, uint32_t *p_uiLenHash);
  * @param uiLenSrc      The length of the source hash string.
  * @param szHashTge     The pointer to the target hash string.
  * @param uiLenTge      The length of the target hash string.
- *
+ * @param p_ucSim       The pointer to the variable which should be updated
+ *                      with the similarity score.
+ * 
  * @return status code
  */
 int8_t
 SimCompareHashPair(char *szHashSrc, uint32_t uiLenSrc, 
-                   char *szHashTge, uint32_t uiLenTge);
+                   char *szHashTge, uint32_t uiLenTge, uint8_t *p_ucSim);
 
 
 #endif
---
+++ plugin/similarity/src/similarity_ssdeep.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <fuzzy.h>
+#include "spew.h"
+#include "similarity.h"
+#include "similarity_ssdeep.h"
+
+
+int8_t
+SimInit()
+{
+    return RC_SUCCESS;
+}
+
+
+int8_t
+SimDeinit()
+{
+    return RC_SUCCESS;
+}
+
+
+int8_t
+SimGetHash(char *szBin, uint32_t uiLenBuf, char **p_szHash, uint32_t *p_uiLenHash)
+{
+    int8_t cRtnCode = RC_SUCCESS;
+
+    *p_szHash = (char*)malloc(sizeof(char) * FUZZY_MAX_RESULT);
+    if (*p_szHash == NULL) {
+        EXIT1(RC_FAIL_MEM_ALLOC, EXIT, "Error: %s.", FAIL_MEM_ALLOC_HASH_STRING);
+    }
+
+    /* Apply ssdeep libaray to covert the binary sequence into fuzzy hash string. */
+    int32_t iStat = fuzzy_hash_buf(szBin, uiLenBuf, *p_szHash);
+    if (iStat != 0) {
+        free(p_szHash);
+        p_szHash = NULL;
+        EXIT1(RC_FAIL_LIBRARY_CALL, EXIT, "Error: %s.", FAIL_EXTERNAL_LIBRARY_CALL);
+    }
+    *p_uiLenHash = strlen(*p_szHash);
+
+EXIT:
+    return cRtnCode;
+}
+
+
+int8_t
+SimCompareHashPair(char *szHashSrc, uint32_t uiLenSrc, 
+                   char *szHashTge, uint32_t uiLenTge, uint8_t *p_ucSim)
+{
+    int8_t cRtnCode = RC_SUCCESS;
+
+    /* Apply ssdeep libaray to compute the similarity between a pair of ssdeep
+       hash strings. */
+    int32_t iStat = fuzzy_compare(szHashSrc, szHashTge);
+    if (iStat == -1) {
+        EXIT1(RC_FAIL_LIBRARY_CALL, EXIT, "Error: %s.", FAIL_EXTERNAL_LIBRARY_CALL);
+    }
+    *p_ucSim = iStat;    
+
+EXIT:
+    return cRtnCode;
+}
