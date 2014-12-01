e60d57546365e5886af9c554c616ddfec6a9b5f0
ZSShen <andy780328@hotmail.com>
2014-12-01 06:29:29 -0800

    Impl-Cluster: Define the major functions.

--- engine/include/cluster.h
+++ engine/include/cluster.h
@@ -2,6 +2,9 @@
 #define _CLUSTER_H_
 
 
+#include <stdint.h>
+
+
 /* This ds records the paramters to control the clustering process. */
 typedef struct _CONFIG_T {
     uint8_t ucCntThrd;      /* The number of threads used for parallel processing. */
@@ -15,9 +18,57 @@ typedef struct _CONFIG_T {
     char *szPluginSlc;      /* The name of the plugin which is to be used for 
                                file slicing. */
     char *szPluginSim;      /* The name of the plugin which is to be used for 
-                               similarity comparison.
-    similarity */
+                               similarity comparison. */
 } CONFIG;
+
+
+/**
+ * This function initializes the clustering engine.
+ *
+ * @return status code
+ */
+int8_t
+ClsInit();
+
+
+/**
+ * This function releases the resources allocated by clustering engine.
+ *
+ * @return status code
+ */
+int8_t
+ClsDeinit();
+
+
+/**
+ * This function sets the task configuration specified by user.
+ *
+ * @param p_Config      The pointer to the user specified configuration
+ *
+ * @return status code
+ */
+int8_t
+ClsSetConfig(CONFIG *p_Config);
+
+
+/**
+ * This function loads two major plugins: 
+ *      1. The plugin for file slicing. 
+ *      2. The plugin for similarity comparison
+ *
+ * @return status code
+ */
+int8_t
+ClsLoadPlugin();
+
+
+/**
+ * This function launches the major clustering workflow.
+ *
+ * @return status code
+ */
+int8_t
+ClsProcessSampleSet();
 
 
 #endif
