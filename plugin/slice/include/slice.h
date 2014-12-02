a230fb8ffc17334dc12143f59ca93f961e8d8781
ZSShen <andy780328@hotmail.com>
2014-12-02 06:49:07 -0800

    Impl-Slice: Code the plugin interface.

--- plugin/slice/include/slice.h
+++ plugin/slice/include/slice.h
@@ -33,6 +33,26 @@ typedef struct _SLICE_T {
 } SLICE;
 
 
+/* The exported interface to interact with this plugin. */
+/* The function pointer types. */
+typedef int8_t (*func_SlcInit) ();
+typedef int8_t (*func_SlcDeinit) ();
+typedef int8_t (*func_SlcGetFileSlice) (char*, uint16_t, GPtrArray**);
+
+/* The integrated structure to store exported functions. */
+typedef struct _PLUGIN_SLICE_T {
+    void *hdle_Lib;
+    func_SlcInit Init;
+    func_SlcDeinit Deinit;
+    func_SlcGetFileSlice GetFileSlice;
+} PLUGIN_SLICE;
+
+/* The function name symbols. */
+#define SYM_SLC_INIT                "SlcInit"
+#define SYM_SLC_DEINIT              "SlcDeinit"
+#define SYM_SLC_GET_FILE_SLICE      "SlcGetFileSlice"
+
+
 /**
  * This function initializes the file slicing plugin.
  *
